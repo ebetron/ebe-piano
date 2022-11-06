80a824feb3f67e95048ffd64c9d2b1be0b85697b
ebetron <ebetron@gmail.com>
2022-11-06 13:11:16 -0500

    Display Note Being Played

--- app.js
+++ app.js
@@ -1,15 +1,44 @@
+const displayMapping = {
+    'c-1' : 'C',
+    'cs-1': 'C#',
+    'd-1': 'D',
+    'ds-1': 'D#',
+    'e-1': 'E',
+    'f-1': 'F',
+    'fs-1': 'F#',
+    'g-1': 'G',
+    'gs-1': 'G#',
+    'a-1': 'A',
+    'as-1': 'A#',
+    'b-1': 'B',
+    'c-2' : 'C',
+    'cs-2': 'C#',
+    'd-2': 'D',
+    'ds-2': 'D#',
+    'e-2': 'E',
+    'f-2': 'F',
+    'fs-2': 'F#',
+    'g-2': 'G',
+    'gs-2': 'G#',
+    'a-2': 'A',
+    'as-2': 'A#',
+    'b-2': 'B',
+};
+
 /* Grab all the keys */
 const pianoKeys = document.querySelectorAll('.key')
 
-/* Initialize Web Audio API to play selected sample */
-const playSound = (url) => {
+/* Plays Sample and Displays Note Played */
+const playSound = (note, url) => {
+    //Play Sound
     new Audio(url).play();
-    console.log(url);
+    //Get note name to display
+    document.getElementById('note').innerHTML = displayMapping[note];
 };
 
 /* Wire Event Listener to each key - use Id to play correct key */
 pianoKeys.forEach((pianoKey) => {
     const pianoKeyId = pianoKey.id;
     const location = `keys/${pianoKeyId}.mp3`;
-    pianoKey.addEventListener('click', () => playSound(location));
+    pianoKey.addEventListener('click', () => playSound(pianoKeyId, location));
 });
