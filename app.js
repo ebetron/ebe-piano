aff7b2c175d95e2b9917a066a5d60a6392e45230
ebetron <ebetron@gmail.com>
2022-11-06 12:44:14 -0500

    Add comments to JS

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
+/* Grab all the keys */
 const pianoKeys = document.querySelectorAll('.key')
 
+/* Initialize Web Audio API to play selected sample */
 const playSound = (url) => {
     new Audio(url).play();
     console.log(url);
 };
 
+/* Wire Event Listener to each key - use Id to play correct key */
 pianoKeys.forEach((pianoKey) => {
     const pianoKeyId = pianoKey.id;
     const location = `keys/${pianoKeyId}.mp3`;
